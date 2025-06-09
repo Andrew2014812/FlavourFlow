f1fb6f8f4ef8a22545e3b8df6a78784315b81757
Andriy Kiyko <kiykoaa07@gmail.com>
2025-06-09 22:41:05 +0300

    update ports

--- api/run.py
+++ api/run.py
@@ -8,11 +8,11 @@ sys.path.append(str(Path(__file__).parent.parent))
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True,
         reload_dirs=["."],
         reload_includes=["*.py"],
         server_header=False,
-        workers=1
+        workers=1,
     )
